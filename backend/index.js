897640b95157bc1fabbe3b055c3410e474fa4a6d
srihari dinesh <dineshdinny>
2024-02-05 10:28:41 +0530

    changet rount

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ const bodyparser=require('body-parser')
 const dotenv=require('dotenv')
 const app=express()
 const chr=require('./routes/chatrout')
-app.use(cors({
-    origin: ["http://localhost:3000", "https://assistantaidinesh.netlify.app/"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-  }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
+    // Continue to the next middleware
+    next();
+  });
 app.use(bodyparser.json());
 dotenv.config()
 app.use('/',chr)
